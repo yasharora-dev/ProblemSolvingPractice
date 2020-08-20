4e3cb0c73a434d439a5b7f2ceedf62dc2b91dff2
Yash Arora <yash.arora310@gmail.com>
2020-08-20 14:12:43 +0530

    Initial Commit

---
+++ aks/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aks
+//
+//  Created by Yash Arora on 20/08/20.
+//  Copyright © 2020 Yash Arora. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
